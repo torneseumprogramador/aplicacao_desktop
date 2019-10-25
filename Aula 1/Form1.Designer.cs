4afe274cabc90e9fea0b321c79767e19693baaa2
Danilo <didox_59@yahoo.com.br>
2019-10-25 08:32:33 -0300

    aula 1

---
+++ Aula 1/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmPessoa
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.lblCpf = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.mskCpf = new System.Windows.Forms.MaskedTextBox();
+            this.mskTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.Location = new System.Drawing.Point(48, 252);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(97, 38);
+            this.btnGravar.TabIndex = 5;
+            this.btnGravar.Text = "Salvar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(45, 70);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(45, 17);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome";
+            this.lblNome.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(112, 65);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(220, 22);
+            this.txtNome.TabIndex = 1;
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Location = new System.Drawing.Point(45, 148);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(64, 17);
+            this.lblTelefone.TabIndex = 3;
+            this.lblTelefone.Text = "Telefone";
+            this.lblTelefone.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblCpf
+            // 
+            this.lblCpf.AutoSize = true;
+            this.lblCpf.Location = new System.Drawing.Point(45, 110);
+            this.lblCpf.Name = "lblCpf";
+            this.lblCpf.Size = new System.Drawing.Size(34, 17);
+            this.lblCpf.TabIndex = 5;
+            this.lblCpf.Text = "CPF";
+            this.lblCpf.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(112, 185);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(220, 22);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(45, 190);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(42, 17);
+            this.lblEmail.TabIndex = 7;
+            this.lblEmail.Text = "Email";
+            this.lblEmail.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // mskCpf
+            // 
+            this.mskCpf.Location = new System.Drawing.Point(112, 105);
+            this.mskCpf.Mask = "999.999.999.99";
+            this.mskCpf.Name = "mskCpf";
+            this.mskCpf.Size = new System.Drawing.Size(220, 22);
+            this.mskCpf.TabIndex = 2;
+            // 
+            // mskTelefone
+            // 
+            this.mskTelefone.Location = new System.Drawing.Point(112, 143);
+            this.mskTelefone.Mask = "(99) 99999-9999";
+            this.mskTelefone.Name = "mskTelefone";
+            this.mskTelefone.Size = new System.Drawing.Size(220, 22);
+            this.mskTelefone.TabIndex = 3;
+            // 
+            // frmPessoa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 354);
+            this.Controls.Add(this.mskTelefone);
+            this.Controls.Add(this.mskCpf);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblCpf);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.btnGravar);
+            this.Name = "frmPessoa";
+            this.Text = "Pessoa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.Label lblCpf;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.MaskedTextBox mskCpf;
+        private System.Windows.Forms.MaskedTextBox mskTelefone;
+    }
+}
+
---
+++ Aula 1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using Business;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmPessoa : Form
+    {
+        public frmPessoa()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnGravar.Visible = true;
+        }
+
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            var pessoa = new Pessoa(){ Nome = txtNome.Text, CPF = mskCpf.Text, Telefone = mskTelefone.Text, Email = txtEmail.Text };
+            pessoa.Salvar();
+
+            MessageBox.Show("Cadastro concluido com sucesso, o seu ID é: " + pessoa.Id);
+        }
+    }
+}
